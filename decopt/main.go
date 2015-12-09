6654f4a5067bfbe57d136e4034d8642966544241
Jon Wear <jon@dramafever.com>
2015-12-08 23:53:56 -0500

    got it

--- decopt/main.go
+++ decopt/main.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 )
 
-var ckey = map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26}
-var outkey = map[int]string{1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"}
+var ckey = map[string]int{"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10, "L": 11, "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17, "S": 18, "T": 19, "U": 20, "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25}
+var outkey = map[int]string{0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H", 8: "I", 9: "J", 10: "K", 11: "L", 12: "M", 13: "N", 14: "O", 15: "P", 16: "Q", 17: "R", 18: "S", 19: "T", 20: "U", 21: "V", 22: "W", 23: "X", 24: "Y", 25: "Z"}
 
 func main() {
 	filesotp, err := ioutil.ReadDir("/Users/jonwear/gocode/src/github.com/genghisjahn/codetalk/makeotp/pads")
@@ -26,9 +26,8 @@ func main() {
 	otpstr := getotp(pathotp)
 	encstr := getenc(pathenc)
 	outputmsg := ""
-	_, _, _ = otpstr, encstr, outputmsg
+
 	for k, v := range encstr {
-		_ = k
 		t := ckey[string(v)]
 		o := ckey[string(otpstr[k])]
 		var d int
@@ -36,11 +35,12 @@ func main() {
 		if d < 0 {
 			d = 26 + d
 		}
+		// fmt.Println(t, o, d, outkey[d])
 		outputmsg += outkey[d]
 	}
 	fmt.Println("Decrypted message:")
 	for k, v := range outputmsg {
-		if (k)%5 == 0 {
+		if (k)%5 == 0 && k != 0 {
 			if (k)%25 == 0 {
 				fmt.Printf("\n")
 			} else {
