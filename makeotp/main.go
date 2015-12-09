b8ee5cca4498717f700f2b8cd291fac2c58f935a
Jon Wear <jon@dramafever.com>
2015-12-08 19:48:38 -0500

    we're making one time pads

--- makeotp/main.go
+++ makeotp/main.go
@@ -4,32 +4,40 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"strconv"
 	"time"
 )
+
+var ckey = map[int]string{1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"}
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	filename := fmt.Sprintf("useonce-%s.otp", time.Now().Format("20060102150405"))
-	otp := [999]int{}
+	otp := [1000]int{}
 	for k := range otp {
 		otp[k] = rand.Intn(26) + 1
 	}
 	makeotp(filename, otp)
 }
 
-func makeotp(path string, otp [999]int) {
+func makeotp(path string, otp [1000]int) {
 	var file *os.File
 	var err error
 	if file, err = os.Create(path); err != nil {
 		panic(err)
 	}
 	defer file.Close()
-
-	for _, elem := range otp {
-		v := strconv.Itoa(elem)
-		fmt.Println(":", v)
-		_, err := file.WriteString(v + "\n")
+	var separator = ""
+	fmt.Println(len(otp))
+	for k, elem := range otp {
+		if (k+1)%5 == 0 {
+			separator = " "
+		}
+		if (k+1)%25 == 0 {
+			separator = "\n"
+		}
+		v := ckey[elem]
+		_, err := file.WriteString(v + separator)
+		separator = ""
 		if err != nil {
 			fmt.Println(err)
 			break
